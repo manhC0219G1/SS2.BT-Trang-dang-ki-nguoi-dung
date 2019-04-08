1d095ebd87c6bceb519ede00e1cef7c67f038ce2
manh <manhvu0107@example.com>
2019-04-08 18:15:38 +0700

    add file

--- BT..php
+++ BT..php
@@ -8,43 +8,45 @@
     <title>Document</title>
 </head>
 <body>
-<form method="post">
-    Name: <input type="text" name="txt1">
-    E-Mail <input type="text" name="txt2">
-    Phone <input type ="text" name = "txt3">
-    <input type="submit" name = "txt4">
+<h1>Registration Form</h1>
+<form action="" method="post">
+    Name: <input type="text" name="txt1" placeholder="Enter Name">
+    E-mail: <input type="text" name="email" placeholder="Enter Email">
+    Phone: <input type="text" name="phone" placeholder="Enter Phone">
+    <input type="submit" value="Register">
 </form>
 <?php
-if($_SERVER["REQUEST_METHOD"]=="POST"){
-    $name = $_POST["txt1"];
-    $email = $_POST["txt2"];
-    $phone = $_POST["txt3"];
-}
-if(empty($name)||empty($email)||empty($phone)){
-    echo "Empty Field";
-}else if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
-    echo "Email Validation is wrong! Try again xxx@xxx.xxx";
-}else {
-    if(file_exists('users.json')){
-        $current_data = file_get_contents('user.json');
-        $array_data = json_decode($current_data, true);
-        $extra = array('name'=>$name,"email"=>$email,"phone"=>$phone);
-        array_push($array_data,$extra);
-        $final_data = json_encode($array_data);
-        file_put_contents('users.json', $final_data);
-        echo "Sucess Added";
-    }else{
-        echo "Json  file not exis";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['txt1'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    // Check input fields
+    if (empty($name) || empty($email) || empty($phone))
+        echo "Empty Field";
+    else if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+        echo "Email Validation is wrong! Try again xxx@xxx.xxx";
+    else {
+        if (file_exists('users.json')) {
+            $current_data = file_get_contents('users.json');
+            //print_r($current_data);
+            $array_data = json_decode($current_data, true);
+            //print_r($array_data);
+            $extra = array(
+                'txt1' => $name,
+                'email' => $email,
+                'phone' => $phone
+            );
+            //print_r($extra);
+            array_push($array_data, $extra);
+            //print_r($array_data);
+            $final_data = json_encode($array_data);
+            file_put_contents('users.json', $final_data);
+            //print_r($final_data);
+            echo "Success Added";
+        } else
+            echo 'JSON File not exits';
     }
-
 }
-function registerUser($name, $email, $phone)
-{
-    $array = [];
-    array_push($array, "$name", "$email", "$phone");
-    return $array;
-}
-
 ?>
 </body>
 </html>
